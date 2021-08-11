29485b6f5aaa2f1acef600dd548d0298e03c1226
Sam Nielsen <sammn721@gmail.com>
2021-08-11 00:24:30 -0700

    everything works now

--- models/Product.js
+++ models/Product.js
@@ -13,14 +13,14 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       validate: {
         isDecimal: true,
