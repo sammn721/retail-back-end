9ae69c1b0098a8200811fa150e673ff10afdcbae
Sam Nielsen <sammn721@gmail.com>
2021-08-10 22:17:31 -0700

    added associations in models/index

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,40 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  const categories = await Category.findAll();
   // be sure to include its associated Products
+  res.json(categories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  const category = await Category.findByPk( req.params.id );
   // be sure to include its associated Products
+  res.json(category);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const category = await Category.create( req.body );
+  res.json(category);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const category = await Category.update( req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.json(category);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const category = await Category.destroy( req.params.id );
+  res.json(category);
 });
 
 module.exports = router;
