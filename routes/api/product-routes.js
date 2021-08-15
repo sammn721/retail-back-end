40c070fc27b7ae5060c73d1501452ad41a2c52a9
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-08-14 22:34:51 -0700

    fixed error in product-routes DEL

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,7 +114,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const product = await Category.destroy({
+    const product = await Product.destroy({
       where: {
         id: req.params.id
       }
